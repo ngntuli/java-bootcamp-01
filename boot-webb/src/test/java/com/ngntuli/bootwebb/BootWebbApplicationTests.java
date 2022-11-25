1fa0018670d07b5d3d12301fe706a7be9a94a239
ngntuli <mr.ng.ntuli@gmail.com>
2022-11-26 00:37:32 +0200

    Application Tester

---
+++ boot-webb/src/test/java/com/ngntuli/bootwebb/BootWebbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngntuli.bootwebb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWebbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
